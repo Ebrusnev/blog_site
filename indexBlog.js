a4c1f848c0324c4be24ec85967cf3dcd1d9b50f0
Ebrusnev <evgenii.brusnev@gmail.com>
2017-05-07 19:57:27 -0500

    Files Updated

---
+++ indexBlog.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var ejs = require('ejs');
+var _ = require('underscore');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+var url = 'mongodb://localhost:27017/myBlog';
+
+app.get('/', function(req, res){
+
+	MongoClient.connect(url, function(err, db) {
+		console.log("Connected correctly to server");
+		db.collection('posts')
+		.find()
+		.toArray(function(err, docs){
+			if (err) {
+				return console.log('Error', err);
+			}
+			db.close();
+			res.render('homePage.ejs', {
+				documents: 	docs
+			})
+		})
+	})
+})
+
+
+app.get('/:id', function(req, res){
+	MongoClient.connect(url, function(err, db){
+		console.log("Connected correctly to server");
+		db.collection('posts')
+		.find({_id: new mongo.ObjectID(req.params.id)})
+		.toArray(function(err, doc){
+			if (err) {
+				db.close();
+				return console.log("Error", err);
+			}
+			console.log(doc, req.params.id);
+			db.close();
+			res.render('post.ejs', {
+				post: doc[0],
+				date: new Date(doc[0].date)
+
+			})
+		})
+	})
+})
+
+
+				//Server listening\\
+
+app.listen(3033, function() {
+	console.log('Server is listenin on port 3033!');
+});
